dd722f81b1d1aae80fe98699ebe6aea1fa692807
Charlotte Chang <yellowspiderlove@gmail.com>
2014-02-09 08:34:02 -0500

    migrating to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140204121125) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: true do |t|
     t.text     "message"
     t.datetime "created_at"
